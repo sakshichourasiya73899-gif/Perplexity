b9e63f7aa5a498fed8f1e1b617a1a8bd42a3a581
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-09-22 01:07:05 +0530

    pushing codes

--- src/Model/user.model.js
+++ src/Model/user.model.js
@@ -70,7 +70,7 @@ const userSchema = new Schema(
 //an already hashed password on unrelated document saves.
 
 userSchema.pre("save",async function(next){
-    if(!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password,10);
-    next();
+  if(!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password,10)
+  next()
 })
